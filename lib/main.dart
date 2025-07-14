2150578387f5501d670be012b1cdb8e74b595c4a
DenUP <ctrike163@gmail.com>
2025-07-14 21:25:08 +0300

    add: Основа

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:supabase_studying/app.dart';
 
-Future<void> main() async {
-  await Supabase.initialize(
-    url: 'https://trxqfvmucjoobomlfbwx.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyeHFmdm11Y2pvb2JvbWxmYnd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3NzI0MjIsImV4cCI6MjAzNjM0ODQyMn0.eX9l2kDDqL5AEXxDpjfMuUiaWZECo2SU2Q5ov4L446E',
-  );
-
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
 }
 
-// Get a reference your Supabase client
-final supabase = Supabase.instance.client;
+class App extends StatelessWidget {
+  const App({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth Supabase',
+    );
+  }
+}
