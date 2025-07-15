06d9e4e25030c5bb2bee962e72b4f061d277c61c
DenUP <ctrike163@gmail.com>
2025-07-15 11:28:53 +0300

    Add: AuthField - Widget

---
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AuthField extends StatelessWidget {
+  final String hintTitle;
+  final TextEditingController controller;
+  final bool isObscureText;
+  const AuthField(
+      {super.key,
+      required this.hintTitle,
+      required this.controller,
+      this.isObscureText = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isObscureText,
+      controller: controller,
+      decoration: InputDecoration(hintText: hintTitle),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Введите $value';
+        }
+      },
+    );
+  }
+}
