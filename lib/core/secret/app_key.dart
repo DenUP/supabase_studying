142646f3f3bbd2f1f5573a5d5e9c2a08dd985fd1
DenUP <ctrike163@gmail.com>
2025-07-16 21:29:39 +0300

    Add: Supabase and Key

---
+++ lib/core/secret/app_key.dart
@@ -0,0 +1,5 @@
+abstract class AppKey {
+  static const url = 'https://ecvvgaeoaaowecmyfdfb.supabase.co';
+  static const anonkey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjdnZnYWVvYWFvd2VjbXlmZGZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MzcyNjMsImV4cCI6MjA2MDMxMzI2M30.eYiGkQ9TLAoah3yaMwSdWdxoPHT5w7jjmbDoFqpDq9Q';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_studying/core/secret/app_key.dart';
 import 'package:supabase_studying/core/theme/app_theme.dart';
 import 'package:supabase_studying/features/auth/presentation/pages/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final supabase =
+      await Supabase.initialize(url: AppKey.url, anonKey: AppKey.anonkey);
   runApp(const App());
 }
 
