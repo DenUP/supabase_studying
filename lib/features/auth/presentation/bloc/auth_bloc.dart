49f5aa30c4770cb7ae9286e08f7df2b18b1cab37
DenUP <ctrike163@gmail.com>
2025-07-16 21:29:24 +0300

    add: Bloc [AuthSigNUP]

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_studying/features/auth/domain/useCases/user_sign_up.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserSignUp _userSignUp;
+  AuthBloc(this._userSignUp) : super(AuthInitial()) {
+    on<SignUpEvent>((event, emit) async {
+      emit(AuthLoading());
+      final userId = await _userSignUp(UserSignUpParam(
+          name: event.name, email: event.email, password: event.password));
+      userId.fold((failure) {
+        emit(AuthFailure(textError: failure.text));
+      }, (userId) {
+        emit(AuthLoaded(userId: userId));
+      });
+    });
+  }
+}
