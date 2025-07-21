a093d09f1120dab6c19e64fc84c8e12d5f4d12d2
DenUP <ctrike163@gmail.com>
2025-07-21 10:49:17 +0300

    modif: Core - UseCases

--- lib/core/useCases/usecase.dart
+++ lib/core/useCases/usecase.dart
@@ -4,3 +4,5 @@ import 'package:supabase_studying/core/error/failure.dart';
 abstract interface class Usecase<Type, Param> {
   Future<Either<Failure, Type>> call(Param param);
 }
+
+class NoParam {}
